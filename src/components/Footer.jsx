cb74108fe9ae86c62b535b63e13ddcba941cf38a
francescocaccia <cacciapuotifrancesco3@gmail.com>
2024-02-07 13:15:33 +0100

    Giacomino carousel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-dark text-white p-3 mt-3 text-center">
+    <footer className="bg-dark text-white py-3 mt-3 text-center">
       <p>© 2023 Pugilistica Fabrianese | Tutti i diritti riservati</p>
     </footer>
   );
--- src/components/MyCarousel.jsx
+++ src/components/MyCarousel.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
 import KoFermo from "../k.o fermo.jpg"; // Assicurati che il percorso sia corretto
-
 import Federico from "../FedericoStroppa.webp";
 import Francesco from "../FrancescoCacciapuoti2.jpg";
 import Federico2 from "../Stroppa.webp";
-
+import Giacomino from "../Giacomino.jpg";
 function MyCarousel() {
   return (
     <Carousel>
@@ -20,6 +19,9 @@ function MyCarousel() {
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={Federico2} alt="Boxing Gloves" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={Giacomino} alt="Boxing Gloves" />
       </Carousel.Item>
     </Carousel>
   );
