156cb30bef879d2823636353cb7a2d8e8c826fb8
francescocaccia <cacciapuotifrancesco3@gmail.com>
2024-02-07 16:54:59 +0100

    Primetto

--- src/components/ChampGallery.jsx
+++ src/components/ChampGallery.jsx
@@ -27,6 +27,24 @@ const ChampGallery = () => {
       <p className="text-center">
         Primo è Presidente Onorario a Vita della nostra palestra
       </p>
+      <p>
+        {" "}
+        <p>
+          "Orgoglio di Fabriano, Primo Zamparini è una leggenda vivente nel
+          mondo della boxe. Nato nella nostra amata città il 9 febbraio 1939,
+          Primo ha scalato le vette dell'Olimpo del pugilato con la passione e
+          la determinazione che solo i veri campioni possiedono. La sua
+          straordinaria carriera è stata coronata da una storica medaglia
+          d'argento nei pesi gallo ai Giochi Olimpici di Roma 1960, un momento
+          indimenticabile che ha elevato lo spirito sportivo di tutta l'Italia.
+          Primo Zamparini non è solo un simbolo di eccellenza sportiva ma anche
+          un esempio di dedizione e impegno, valori che continuano a ispirare
+          giovani atleti nella nostra comunità e oltre. La sua eredità vive
+          nella nostra palestra, dove Primo, con grande onore, ricopre il ruolo
+          di Presidente Onorario a Vita, la sua immagine guida con passione le
+          nuove generazioni verso la grandezza."
+        </p>
+      </p>
       <div className="row">
         {images.map((image) => (
           <div key={image.id} className="col-12 col-sm-6 col-md-4 mb-4">
